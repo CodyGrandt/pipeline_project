4032b2ff3073c34b9c55b23d564fb93c2b2c2701
CodyGrandt <CodyGrandt@users.noreply.github.com>
2024-03-26 00:36:00 +0000

    deploy: 20c880bb2ea87b1d80d3427198377446a5949d06

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "0050ff900dd5faec1f424c2c5fe8c26f",
-"/": "0050ff900dd5faec1f424c2c5fe8c26f",
+const RESOURCES = {"index.html": "b3dbdec022daaa2b31138a28e662fde9",
+"/": "b3dbdec022daaa2b31138a28e662fde9",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
