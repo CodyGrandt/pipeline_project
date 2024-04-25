3c1489c55e4bec11da2e80d20fd5372b462339b1
CodyGrandt <codygrandt04@gmail.com>
2024-04-25 14:20:21 -0400

    app built besides search

--- flutter_todo_app/lib/screens/home.dart
+++ flutter_todo_app/lib/screens/home.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo_app/constants/colors.dart';
 import '../widgets/todo_item.dart';
+import '../model/todo.dart';
 
-class Home extends StatelessWidget {
-  const Home ( { Key? key } ) : super ( key: key );
+class Home extends StatefulWidget {
+  Home ( { Key? key } ) : super ( key: key );
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final todosList = ToDo.todoList ( );
+  final _todoController = TextEditingController ( );
 
   @override
   Widget build ( BuildContext context ) {
@@ -11,42 +20,127 @@ class Home extends StatelessWidget {
     return Scaffold ( 
     backgroundColor : tdBGColor,
     appBar: _buildAppBar(),
-    body : Container ( 
-      padding : EdgeInsets.symmetric ( 
-
-        horizontal : 20 , 
-        vertical : 15 
+    body : Stack (
+      children: [
+        Container ( 
+          padding : EdgeInsets.symmetric ( 
         
-        ),
-      
-      child : Column (
-        children: [
-          searchBox ( ),
-          Expanded (
-            child: ListView ( 
-              children : [
-                Container ( 
+            horizontal : 20 , 
+            vertical : 15 
+            
+            ),
+          
+          child : Column (
+            children: [
+              searchBox ( ),
+              Expanded (
+                child: ListView ( 
+                  children : [
+                    Container ( 
+                      margin : EdgeInsets.only (
+                        top : 50,
+                        bottom : 20,
+                      ),
+                      child : Text (
+                        'All ToDos',
+                        style : TextStyle (
+                          fontSize : 30,
+                          fontWeight : FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    for ( ToDo todoo in todosList )
+                      ToDoItem (
+                        todo : todoo, 
+                        onToDoChanged : _handleToDoChange,
+                        onDeleteItem : _deleteToDoItem,
+                      ),
+                  ],
+                ),
+              ),
+            ],
+           ),
+          ),
+          Align (
+            alignment : Alignment.bottomCenter,
+            child : Row ( children : [
+              Expanded (
+                child : Container (
                   margin : EdgeInsets.only (
-                    top : 50,
                     bottom : 20,
+                    right : 20,
+                    left : 20,
                   ),
-                  child : Text (
-                    'All ToDos',
-                    style : TextStyle (
-                      fontSize : 30,
-                      fontWeight : FontWeight.w500,
+                  padding : EdgeInsets.symmetric (
+                    horizontal : 20,
+                    vertical : 5,
+                  ),
+                  decoration : BoxDecoration (
+                    color : Colors.white,
+                    boxShadow : const [ BoxShadow (
+                      color : Colors.grey,
+                      offset : Offset ( 0.0 , 0.0 ),
+                      blurRadius : 10.0,
+                      spreadRadius : 0.0,
+                    ),],
+                    borderRadius : BorderRadius.circular ( 10 ),
+                  ),
+                  child : TextField (
+                    controller : _todoController,
+                    decoration : InputDecoration (
+                      hintText : 'Add a new todo item',
+                      border : InputBorder.none,
                     ),
                   ),
+                ), 
+              ),
+              Container (
+                margin : EdgeInsets.only (
+                  bottom : 20,
+                  right : 20,
                 ),
-                ToDoItem ( ),
-              ],
-            ),
-          ),
-        ],
-       ),
-      ),
-    );
+                child : ElevatedButton (
+                  child : Text ( '+' , style : TextStyle ( fontSize : 40 ), ),
+                  onPressed : () {
+                    _addToDoItem( _todoController.text );
+                  },
+                  style : ElevatedButton.styleFrom (
+                    backgroundColor: tdBlue,
+                    minimumSize : Size ( 60 , 60 ),
+                    elevation : 10,
+                  ),
+                ),
+              ),
+            ]),
+          )
+       ],
+    ),
+   );
   }
+
+  void _handleToDoChange ( ToDo todo ) {
+    setState ( () {
+    todo.isDone = !todo.isDone;
+    });
+  }
+  
+  void _deleteToDoItem ( String id ) {
+    setState ( () {
+    todosList.removeWhere ( ( item ) => item.id == id );
+    });
+  }
+
+  void _addToDoItem ( String toDo ) {
+    setState ( () {
+    todosList.add ( ToDo ( 
+      id : DateTime.now ( ).millisecondsSinceEpoch.toString ( ) , 
+      todoText : toDo,
+      ));
+    });
+    _todoController.clear ( );
+  }
+
+
 
   Widget searchBox ( ) {
    
@@ -79,7 +173,6 @@ class Home extends StatelessWidget {
       ),
     );
   }
-
 
   AppBar _buildAppBar() {
     return AppBar ( 
--- flutter_todo_app/lib/widgets/todo_item.dart
+++ flutter_todo_app/lib/widgets/todo_item.dart
@@ -1,25 +1,60 @@
 import 'package:flutter/material.dart';
 import '../constants/colors.dart';
+import '../model/todo.dart';
 
 class ToDoItem extends StatelessWidget {
-  const ToDoItem ( { Key? key } ) : super ( key : key );
+  final ToDo todo;
+  final onToDoChanged;
+  final onDeleteItem;
+
+  const ToDoItem ( { Key? key , required this.todo , required this.onToDoChanged , required this.onDeleteItem } ) : super ( key : key );
+
 
   @override
   Widget build ( BuildContext context ) {
     return Container ( 
+      margin : EdgeInsets.only (
+        bottom : 20,
+        ),
       child : ListTile (
-        onTap : () {},
+        onTap : () {
+          // print ( 'Clicked on Todo Item.' );
+          onToDoChanged ( todo );
+        },
         shape : RoundedRectangleBorder (
           borderRadius : BorderRadius.circular ( 20 ),   
         ),
+        contentPadding : EdgeInsets.symmetric ( horizontal : 20 , vertical : 5 ),
         tileColor : Colors.white,
-        leading : Icon ( Icons.check_box, color : tdBlue, ),
+        leading : Icon ( 
+          todo.isDone? Icons.check_box : Icons.check_box_outline_blank, 
+          color : tdBlue, 
+          ),
         title : Text ( 
-          'Check Mail'
+          todo.todoText!,
           style : TextStyle (
             fontSize : 16,
             color : tdBlack,
-            decoration : TextDecoration.lineThrough,
+            decoration : todo.isDone? TextDecoration.lineThrough : null,
+          ),
+        ),
+        trailing : Container (
+          padding : EdgeInsets.all ( 0 ),
+          margin : EdgeInsets.symmetric ( vertical : 12 ),
+          height : 35,
+          width : 35,
+          decoration : BoxDecoration (
+            color : tdRed,
+            borderRadius : BorderRadius.circular ( 5 ),
+          ),
+          child : IconButton (
+            color : Colors.white,
+            iconSize : 18,
+            icon : Icon ( Icons.delete ),
+            onPressed : () {
+              //print ( 'Clicked on delete icon' );
+              onDeleteItem ( todo.id );
+            },
           ),
         ),
       ),
