7c9f2bcc9d759e05f13ffbffa3c231f06e9abb6f
CodyGrandt <codygrandt04@gmail.com>
2024-04-24 20:50:26 -0400

    todo app begun

---
+++ flutter_todo_app/lib/screens/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home ( { Key? key } ) : super ( key: key );
+
+  @override
+  Widget build ( BuildContext context ) {
+
+    return Scaffold ( 
+
+      appBar: AppBar ( title : Text ( 'ToDo App ' ) ),
+      body : Container ( 
+
+        child : Text ( 'This is home screen. '),
+      ),
+    );
+  }
+}
